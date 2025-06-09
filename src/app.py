ab9b5e8553b0bbc9319c69467ce60e5058b1a3d9
Dabeldab <1dareios@gmail.com>
2025-06-09 23:06:10 +0000

    delete function pending

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+todos = [
+    { "label": "My first task", "done": False, "id": 1},
+    { "label": "My second task", "done": False, "id": 2},
+]
+    
+@app.route('/todos', methods=['GET'])
+def hello_world():    
+    return jsonify(todos)
+    
+@app.route('/', methods=['GET'])
+def hello():
+    return "<h1>Hello World</>"
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return 'Response for the POST todo'
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    
+    print("This is the position to delete:", position)
+    return 'something'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =3245, debug=True)
+    
